23dd920d57660ca11ea6e41c107d5cd3884198e2
PraneethMalshan <praneethmalshan12345@gmail.com>
2023-09-15 15:17:34 +0530

    add Controllers

---
+++ POS_FrontEnd/controller/CustomerController.js
@@ -0,0 +1,140 @@
+let baseUrl="http://localhost:8082/posapplayerd/"
+
+
+getAllCustomers();  //Formeka enakota customersla tika load welaa thiyenna
+
+
+//NuttonEvents
+//Add Customer
+$("#btnCustomer").click(function () {
+    let formData = $("#customerForm").serialize();
+
+    $.ajax({
+        url:baseUrl+"customer",
+        method:"post",
+        data:formData,
+        dataType: "json",
+        success:function (res) {
+            //Invoked if the response status code is in 200 range
+            console.log("Success Method Invoked");
+            console.log(res);
+            alert(res.message);
+            getAllCustomers();
+        },
+        error:function (error) {
+            //Invokes if status code range is 500 range or 400 range
+            console.log("Error Method Invoked");
+            console.log(JSON.parse(error.responseText));
+            alert(JSON.parse(error.responseText).message);
+
+        }
+    });
+
+});
+
+
+//Delete customer
+
+$("#btnCusDelete").click(function () {
+    let id = $("#txtCustomerID").val();
+    $.ajax({
+        url:baseUrl+"customer?id="+id,
+        method:"delete",
+        success:function (resp) {
+            getAllCustomers();
+            alert(resp.message);
+        },
+        error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+});
+
+//Update customer
+$("#btnUpdate").click(function () {
+    let cusId = $("#txtCustomerID").val();
+    let cusName = $("#txtCustomerName").val();
+    let cusAddress = $("#txtCustomerAddress").val();
+    let cusSalary = $("#txtCustomerSalary").val();
+
+    var customerOb={
+        id:cusId,
+        name:cusName,
+        address:cusAddress,
+        salary:cusSalary
+    }
+
+    $.ajax({
+        url:baseUrl+"customer",
+        method:"put",
+        contentType:"application/json",  //JS Object ekak JSON ekak widiyata yawanawa nam meya daanna oona
+        data: JSON.stringify(customerOb),   //JS Object ekak JSON ekak widiyata yawanawa nam aniwaaryayen "   JSON.stringify()  ", ekata daala ywanna oona..
+        dataType: "json",
+        success:function (resp) {
+            getAllCustomers();
+            alert(resp.message);
+        },
+        error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+});
+
+//GetAll customer
+$("#btnGetAll").click(function () {
+    getAllCustomers();
+});
+//GetAll customer function
+function getAllCustomers() {
+    $("#tblCustomer").empty();
+    $.ajax({
+        url:baseUrl+"customer",
+        success:function (res) {
+            // console.log(res)
+            for (let c of res.data) {
+                let cusID=c.id;
+                let cusName=c.name;
+                let cusAddress=c.address;
+                let cusSalary=c.salary;
+
+                let row= "<tr><td>"+cusID+"</td><td>"+cusName+"</td><td>"+cusAddress+"</td><td>"+cusSalary+"</td></tr>";
+                $("#tblCustomer").append(row);
+            }
+            bindRowClickEvents();
+            setTextFieldValues("","","","");
+        },
+        error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
+
+
+//Bind events for the table rows function
+function bindRowClickEvents() {
+    $("#tblCustomer>tr").click(function () {
+        let id = $(this).children(":eq(0)").text();
+        let name = $(this).children(":eq(1)").text();
+        let address = $(this).children(":eq(2)").text();
+        let salary = $(this).children(":eq(3)").text();
+
+        //Setting table details values to text fields
+
+        $('#txtCustomerID').val(id);
+        $('#txtCustomerName').val(name);
+        $('#txtCustomerAddress').val(address);
+        $('#txtCustomerSalary').val(salary);
+    });
+}
+
+//Set text fields values function
+function setTextFieldValues(id, name, address, salary) {
+    $("#txtCustomerID").val(id);
+    $("#txtCustomerName").val(name);
+    $("#txtCustomerAddress").val(address);
+    $("#txtCustomerSalary").val(salary);
+}
+
---
+++ POS_FrontEnd/controller/ItemController.js
@@ -0,0 +1,138 @@
+// let baseUrl="http://localhost:8082/posapplayerd/"
+
+
+
+getAllItems();  //Formeka enakota customersla tika load welaa thiyenna
+
+
+//NuttonEvents
+//Add Item
+$("#btnItem").click(function () {
+    let formData = $("#itemForm").serialize();
+
+    $.ajax({
+        url:baseUrl+"item",
+        method:"post",
+        data:formData,
+        dataType: "json",
+        success:function (res) {
+
+            alert(res.message);
+            getAllItems();
+        },
+        error:function (error) {
+            //Invokes if status code range is 500 range or 400 range
+            console.log("Error Method Invoked");
+            console.log(JSON.parse(error.responseText));
+            alert(JSON.parse(error.responseText).message);
+
+        }
+    });
+
+});
+
+
+//Delete Item
+
+$("#btnItemDelete").click(function () {
+    let code = $("#itemCode").val();
+    $.ajax({
+        url:"http://localhost:8082/posapplayerditem?code="+code,
+        method:"delete",
+        success:function (resp) {
+            getAllItems();
+            alert(resp.message);
+        },
+        error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+});
+
+//Update Item
+$("#btnItemUpdate").click(function () {
+    let code = $("#itemCode").val();
+    let name = $("#itemName").val();
+    let qty = $("#itemQty").val();
+    let price = $("#itemPrice").val();
+
+    var itemOb={
+        code:code,
+        description:name,
+        qtyOnHand:qty,
+        unitPrice:price
+    }
+
+    $.ajax({
+        url:"http://localhost:8082/posapplayerditem",
+        method:"put",
+        contentType:"application/json",  //JS Object ekak JSON ekak widiyata yawanawa nam meya daanna oona
+        data: JSON.stringify(itemOb),   //JS Object ekak JSON ekak widiyata yawanawa nam aniwaaryayen "   JSON.stringify()  ", ekata daala ywanna oona..
+        dataType: "json",
+        success:function (resp) {
+            getAllItems();
+            alert(resp.message);
+        },
+        error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+});
+
+//GetAll Item
+$("#btnItemGetAll").click(function () {
+    getAllItems();
+});
+//GetAll customer function
+function getAllItems() {
+    $("#tblItem").empty();
+    $.ajax({
+        url:"http://localhost:8082/posapplayerd/item",
+        success:function (res) {
+            // console.log(res)
+            for (let c of res.data) {
+                let code=c.code;
+                let description=c.description;
+                let qtyOnHand=c.qtyOnHand;
+                let unitPrice=c.unitPrice;
+
+                let row= "<tr><td>"+code+"</td><td>"+description+"</td><td>"+qtyOnHand+"</td><td>"+unitPrice+"</td></tr>";
+                $("#tblItem").append(row);
+            }
+            bindRowClickEvents();
+            setTextFieldValues("","","","");
+        },
+        error:function (error) {
+            let message= JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
+
+
+//Bind events for the table rows function
+function bindRowClickEvents() {
+    $("#tblItem>tr").click(function () {
+        let code = $(this).children(":eq(0)").text();
+        let name = $(this).children(":eq(1)").text();
+        let qty = $(this).children(":eq(2)").text();
+        let price = $(this).children(":eq(3)").text();
+
+        //Setting table details values to text fields
+
+        $('#itemCode').val(code);
+        $('#itemName').val(name);
+        $('#itemQty').val(qty);
+        $('#itemPrice').val(price);
+    });
+}
+
+//Set text fields values function
+function setTextFieldValues(code, name, qty, price) {
+    $('#itemCode').val(code);
+    $('#itemName').val(name);
+    $('#itemQty').val(qty);
+    $('#itemPrice').val(price);
+}
